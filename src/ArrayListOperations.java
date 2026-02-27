f3e4fb64f854bb574f307d57e75b22e78caa54ce
amp-06 <tve24el008@cet.ac.in>
2026-02-27 11:04:42 +0530

    Update ArrayListOperations.java

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -2,31 +2,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class ArrayListOperations {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();   // Number of items
-        scanner.nextLine();          // Consume newline
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
         ArrayList<String> items = new ArrayList<>();
 
-        // Read items
         String[] inputItems = scanner.nextLine().split(" ");
         for (int i = 0; i < n; i++) {
             items.add(inputItems[i]);
         }
 
-        // Sort alphabetically
         Collections.sort(items);
 
-        // Read search item
         String searchItem = scanner.nextLine();
 
-        // Print sorted list
         System.out.println("Sorted Items: " + items);
 
-        // Check existence
         if (items.contains(searchItem)) {
             System.out.println("Found");
         } else {
